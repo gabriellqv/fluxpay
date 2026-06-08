a8411c6d3ea8c2d245046fdf9ed064f288bb814c
Gabriel Queiroz <gabriellqv@gmail.com>
2026-06-08 17:55:04 -0300

    feat: add user repository for database operations

---
+++ src/modules/users/users.repository.ts
@@ -0,0 +1,42 @@
+import { prisma } from '../../config/prisma';
+import { CreateUserDTO, UpdateUserDTO } from './users.dtos';
+export class UserRepository {
+  async findById(id: string) {
+    return prisma.user.findUnique({
+      where: { id },
+    });
+  }
+  async findByCpf(cpf: string) {
+    return prisma.user.findUnique({
+      where: { cpf },
+    });
+  }
+  async findByEmail(email: string) {
+    return prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  async create(data: CreateUserDTO) {
+    return prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        cpf: data.cpf,
+        password: data.password,
+      },
+    });
+  }
+
+  async update(id: string, data: UpdateUserDTO) {
+    return prisma.user.update({
+      where: { id },
+      data: {
+        name: data.name,
+        email: data.email,
+        cpf: data.cpf,
+        password: data.password,
+      },
+    });
+  }
+}
