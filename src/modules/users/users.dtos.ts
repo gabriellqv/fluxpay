cb91981e21372cfb6c7f30cca669367c993d8f2f
Gabriel Queiroz <gabriellqv@gmail.com>
2026-06-08 16:11:28 -0300

    feat: add user validation DTOs

---
+++ src/modules/users/users.dtos.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+  name: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres!'),
+  email: z.string().email('Email Inválido!'),
+  cpf: z
+    .string()
+    .length(11, 'CPF deve ter exatamente 11 dígitos numéricos')
+    .regex(/^\d+$/, 'CPF deve conter apenas números'),
+  password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
+});
+
+export type CreateUserDTO = z.infer<typeof createUserSchema>;
+
+export const updateUserSchema = createUserSchema.partial();
+export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
+
+export const replaceUserSchema = createUserSchema;
+export type ReplaceUserDTO = z.infer<typeof replaceUserSchema>;
