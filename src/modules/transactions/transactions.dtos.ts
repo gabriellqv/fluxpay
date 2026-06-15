5825df8e7944ff5a3bd43b5a51c6e018e62f8b12
Gabriel Queiroz <102629497+gabriellqv@users.noreply.github.com>
2026-06-15 17:43:49 -0300

    feat(swagger): implement automated openapi docs with zod (#7)

--- src/modules/transactions/transactions.dtos.ts
+++ src/modules/transactions/transactions.dtos.ts
@@ -1,13 +1,22 @@
 import { z } from 'zod';
+import { registry } from '../../config/swagger';
 
-export const createTransactionSchema = z.object({
-  senderId: z.string().uuid({ message: 'O ID do remetente (senderId) deve ser um UUID válido.' }),
-
-  receiverId: z
-    .string()
-    .uuid({ message: 'O ID do destinatário (receiverId) deve ser um UUID válido.' }),
-
-  amount: z.number().positive({ message: 'O valor da transferência deve ser maior que zero.' }),
-});
+export const createTransactionSchema = registry.register(
+  'CreateTransaction',
+  z.object({
+    senderId: z
+      .string()
+      .uuid({ message: 'O ID do remetente (senderId) deve ser um UUID válido.' })
+      .openapi({ example: '123e4567-e89b-12d3-a456-426614174000' }),
+    receiverId: z
+      .string()
+      .uuid({ message: 'O ID do destinatário (receiverId) deve ser um UUID válido.' })
+      .openapi({ example: '987fcdeb-51a2-43d7-9012-345678912000' }),
+    amount: z
+      .number()
+      .positive({ message: 'O valor da transferência deve ser maior que zero.' })
+      .openapi({ example: 50.5 }),
+  }),
+);
 
 export type CreateTransactionDTO = z.infer<typeof createTransactionSchema>;
