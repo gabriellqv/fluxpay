fcf18d7d5ed83d9279683467f69f849992924ae2
Gabriel Queiroz <gabriellqv@gmail.com>
2026-06-16 19:47:09 -0300

    feat(security): integrate helmet, cors and rate limiter in app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { generateOpenApiDocument } from './config/swagger';
 import { errorHandler } from './middlewares/errorHandler';
+import { globalRateLimiter, sensitiveRateLimiter } from './middlewares/rateLimiter';
 import { transactionsRoutes } from './modules/transactions/transactions.routes';
 import { usersRoutes } from './modules/users/users.routes';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+app.use(globalRateLimiter);
 
 const swaggerDocument = generateOpenApiDocument();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', usersRoutes);
-app.use('/transactions', transactionsRoutes);
+app.use('/transactions', sensitiveRateLimiter, transactionsRoutes);
 
 app.get(['/', '/health'], (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Flux Pay API is running' });
